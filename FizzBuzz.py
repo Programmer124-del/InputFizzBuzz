daf3d4f6501b7f020c665609e1f846eeab26994c
Programmer124-del <71405361+Programmer124-del@users.noreply.github.com>
2020-09-28 19:00:04 +0100

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,14 @@
+num = int(input("Please enter a number: "))
+
+def fizz_buzz():
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+
+
+print(fizz_buzz())
